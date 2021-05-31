72baed9edea49eadc88669842030522d9bd33460
Abed <id.abed@gmail.com>
2021-05-31 18:22:34 +0300

    added pitch detection with pitch ranges

--- combi.py
+++ combi.py
@@ -10,9 +10,10 @@ import aubio
 import numpy as np
 import pyaudio
 import sys
-
 import pyaudio
 import aubio
+
+import datetime
 
 # Some constants for setting the PyAudio and the
 # Aubio.
@@ -23,8 +24,11 @@ METHOD                  = "default"
 SAMPLE_RATE             = 44100
 HOP_SIZE                = BUFFER_SIZE//2
 PERIOD_SIZE_IN_FRAME    = HOP_SIZE
-
-
+SECONDS_IN_MINUT        = 60
+LENGTH_OF_TIME          = 5
+THESHHOLD               = 0.0
+pitchList               = [35,45,55,65,75,85]
+PITCH_COUNTER_THRESH    = 3
 
 def main(args):
 
@@ -34,16 +38,6 @@ def main(args):
     mic = pA.open(format=FORMAT, channels=CHANNELS,
         rate=SAMPLE_RATE, input=True,
         frames_per_buffer=PERIOD_SIZE_IN_FRAME)
-
-#     # Initiating Aubio's pitch detection object.
-#     pDetection = aubio.pitch(METHOD, BUFFER_SIZE,
-#         HOP_SIZE, SAMPLE_RATE)
-#     # Set unit.
-#     pDetection.set_unit("Hz")
-#     # Frequency under -40 dB will considered
-#     # as a silence.
-#     pDetection.set_silence(-40)
-#
 
     
     # setup pitch
@@ -55,8 +49,11 @@ def main(args):
     pitch_o.set_tolerance(tolerance)
     
     # Infinite loop!
+    past = datetime.datetime.now()
+    pitchlist=[]
+    pitchcounter=[0,0,0,0,0,0]
     while True:
-
+        
         # Always listening to the microphone.
         data = mic.read(BUFFER_SIZE)
         # Convert into number that Aubio understand.
@@ -69,17 +66,47 @@ def main(args):
 
         pitch = pitch_o(signal)[0]
         confidence = pitch_o.get_confidence()
-            
+        
+        if(pitch >  THESHHOLD):
+            pitchlist.append(pitch)
         # Compute the energy (volume)
         # of the current frame.
         volume = np.sum(samples**2)/len(samples)
         # Format the volume output so it only
         # displays at most six numbers behind 0.
         volume = "{:6f}".format(volume)
-
-        # Finally print the pitch and the volume.
-        print(str(volume))
-        print(" volume = {} / pitch = {} / confidence  ={}".format(str(volume),pitch,confidence))
+        
+        
+        present = datetime.datetime.now()
+        difference = present - past
+        
+        # Finally print the pitch and the volume if time length threshold passed.
+        if(divmod(difference.total_seconds(), SECONDS_IN_MINUT)[1] >= LENGTH_OF_TIME):
+            #print(" volume = {} / pitch = {} / confidence  ={}".format(str(volume),pitch,confidence))
+            
+            print(pitchlist)
+            
+            #if no pitch was detected then do not print pitches
+            if(len(pitchlist) != 0):
+                for value in pitchlist:
+                    
+                    #round values to clossest pre defined pitch value
+                    index = int(value)//10 -3
+                    
+                    #check if value is within range
+                    if(index>=0 and index <=5):
+                        pitchcounter[index] += 1
+            print(pitchcounter)
+            max_value = max(pitchcounter)
+            max_value_index=pitchcounter.index(max_value)
+            #if max value counter is heigher than threshold then print pitch
+            if(max_value > PITCH_COUNTER_THRESH):
+                print("pitch is " + str(pitchList[max_value_index]))
+                
+            #reset values of pitchlist and pitch counter
+            pitchlist=[]
+            pitchcounter=[0,0,0,0,0,0]
+            past = present
 
 if __name__ == "__main__": main(sys.argv)
 
